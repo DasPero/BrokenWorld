16adf6959a0b04b731e0958235f330118d2b0eee
Filip Simic <filipsimic25@gmail.com>
2020-03-09 22:13:53 +0100

    Dodal Menije

---
+++ BrokenWorld/Assets/Scripts/RainControler.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RainControler : MonoBehaviour
+{
+	public Texture2D[] frames;
+	public int framesPerSecond = 10;
+
+	// Start is called before the first frame update
+	void Start()
+    {
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+		int index = (int)((Time.time * framesPerSecond) % frames.Length);
+		GetComponent<Renderer>().material.mainTexture = frames[index];
+	}
+}
